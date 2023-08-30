7349a4cd23e667e954d236097a017583f7e22357
Adhi Setiawan <adhisetiawan518@gmail.com>
2023-08-30 22:21:53 +0700

    modified buffer size and training step

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3 dqn_atari.py \
-    --exp-name Qbert \
+    --exp-name MsPacman-v5 \
     --track \
     --wandb-project-name ALE \
     --capture-video \
-    --env-id ALE/Qbert-v5 \
+    --env-id ALE/MsPacman-v5 \
     --total-timesteps 5000000 \
     --buffer-size 400000 \
     --save-model \
